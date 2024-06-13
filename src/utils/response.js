59bc655c6772b34f18d2fb966d4728cadfd140b4
magdybadr978 <magdibadr2002@gmail.com>
2024-06-14 02:52:22 +0300

    add APIResponse

---
+++ src/utils/response.js
@@ -0,0 +1,11 @@
+class ApiResponse {
+  static success(res, data, message = 'Success', statusCode = 200) {
+    return res.status(statusCode).json({
+      success: true,
+      message,
+      data
+    });
+  }
+}
+
+export default ApiResponse;
