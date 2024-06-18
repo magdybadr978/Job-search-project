774869c85f2cae47fc1e347be92cd68731bbbff6
magdybadr978 <magdibadr2002@gmail.com>
2024-06-18 22:08:02 +0300

    add response class in utils

--- src/utils/response.js
+++ src/utils/response.js
@@ -1,11 +1,21 @@
+// ApiResponse
 class ApiResponse {
+  // success response
   static success(res, data, message = 'Success', statusCode = 200) {
     return res.status(statusCode).json({
       success: true,
       message,
-      data
+      data,
+    });
+  }
+ // failed response
+  static error(res, message = 'Error', statusCode = 500) {
+    return res.status(statusCode).json({
+      success: false,
+      message,
     });
   }
 }
 
 export default ApiResponse;
+
